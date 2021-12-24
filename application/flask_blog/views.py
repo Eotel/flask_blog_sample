7353da5e1051981a25359ac3e62a83bc854abca3
eotel <usawok.4149@gmail.com>
2021-12-25 07:32:45 +0900

    feat: Changed to return index.html

--- application/flask_blog/views.py
+++ application/flask_blog/views.py
@@ -1,6 +1,8 @@
+from flask import flash, redirect, render_template, request, session, url_for
+
 from . import app
 
 
 @app.route("/")
 def show_entries():
-    return "Hello World!"
+    return render_template("entries/index.html")
